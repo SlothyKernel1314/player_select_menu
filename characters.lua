dcf224eaddef62694ebc0327da51f6e0b711eca3
IAmTerror <cyrilberthout@gmail.com>
2022-03-13 00:40:35 +0100

    characters.lua change structure

--- characters.lua
+++ characters.lua
@@ -1,21 +1,22 @@
 local characters = {}
 
-characters.ryu = {1, "Ryu", "Japan"}
-characters.ken = {2, "Ken Masters", "U.S.A."}
-characters.honda = {3, "E. Honda", "Japan"}
-characters.chunli = {4, "Chun Li", "China"}
-characters.blanka = {5, "Blanka", "Brazil"}
-characters.zangief = {6, "Zangief", "U.S.S.R."}
-characters.guile = {7, "Guile", "U.S.A."}
-characters.dhaslim = {8, "Dhaslim", "India"}
-characters.hawk = {9, "T. Hawk", "Mexico"}
-characters.cammy = {10, "Cammy", "England"}
-characters.feilong = {11, "Fei Long", "Hong Kong"}
-characters.deejay = {12, "Dee Jay", "Jamaica"}
-characters.balrog = {13, "Balrog", "U.S.A."}
-characters.vega = {14, "Vega", "Spain"}
-characters.sagat = {15, "Sagat", "Thailand"}
-characters.bison = {16, "M. Bison", "Thailand"}
-characters.akuma = {17, "Akuma", "Japan"}
+characters.roster = {}
+characters.roster.ryu = {1, "Ryu", "Japan"}
+characters.roster.ken = {2, "Ken Masters", "U.S.A."}
+characters.roster.honda = {3, "E. Honda", "Japan"}
+characters.roster.chunli = {4, "Chun Li", "China"}
+characters.roster.blanka = {5, "Blanka", "Brazil"}
+characters.roster.zangief = {6, "Zangief", "U.S.S.R."}
+characters.roster.guile = {7, "Guile", "U.S.A."}
+characters.roster.dhaslim = {8, "Dhaslim", "India"}
+characters.roster.hawk = {9, "T. Hawk", "Mexico"}
+characters.roster.cammy = {10, "Cammy", "England"}
+characters.roster.feilong = {11, "Fei Long", "Hong Kong"}
+characters.roster.deejay = {12, "Dee Jay", "Jamaica"}
+characters.roster.balrog = {13, "Balrog", "U.S.A."}
+characters.roster.vega = {14, "Vega", "Spain"}
+characters.roster.sagat = {15, "Sagat", "Thailand"}
+characters.roster.bison = {16, "M. Bison", "Thailand"}
+characters.roster.akuma = {17, "Akuma", "Japan"}
 
 return characters
