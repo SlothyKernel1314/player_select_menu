8b071c6cc060247188a442ea7815c32f874d228e
IAmTerror <cyrilberthout@gmail.com>
2022-03-12 22:52:27 +0100

    character data

---
+++ characters.lua
@@ -0,0 +1,21 @@
+local characters = {}
+
+characters.ryu = {1, "Ryu", "Japan"}
+characters.ken = {2, "Ken Masters", "U.S.A."}
+characters.honda = {3, "E. Honda", "Japan"}
+characters.chunli = {4, "Chun Li", "China"}
+characters.blanka = {5, "Blanka", "Brazil"}
+characters.zangief = {6, "Zangief", "U.S.S.R."}
+characters.guile = {7, "Guile", "U.S.A."}
+characters.dhaslim = {8, "Dhaslim", "India"}
+characters.hawk = {9, "T. Hawk", "Mexico"}
+characters.cammy = {10, "Cammy", "England"}
+characters.feilong = {11, "Fei Long", "Hong Kong"}
+characters.deejay = {12, "Dee Jay", "Jamaica"}
+characters.balrog = {13, "Balrog", "U.S.A."}
+characters.vega = {14, "Vega", "Spain"}
+characters.sagat = {15, "Sagat", "Thailand"}
+characters.bison = {16, "M. Bison", "Thailand"}
+characters.akuma = {17, "Akuma", "Japan"}
+
+return characters
