57852d813bc334f01143533fd5414f41783b696d
IAmTerror <cyrilberthout@gmail.com>
2022-03-13 18:43:48 +0100

    config project (import tiles, SNES resolution, etc...)

--- game_settings.lua
+++ game_settings.lua
@@ -2,4 +2,10 @@ local game_settings = {}
 
 game_settings.max_characters = 16
 
+game_settings.screen_width = 256
+game_settings.screen_height = 224
+game_settings.screen_ratio = game_settings.screen_width / game_settings.screen_height
+
+game_settings.scale_factor = 3
+
 return game_settings
--- main.lua
+++ main.lua
@@ -3,6 +3,10 @@
 --|I|A|m|T|e|r|r|o|r|
 --+-+-+-+-+-+-+-+-+-+
 
+-- Street Fighter Wiki - Character Select : https://streetfighter.fandom.com/wiki/Character_Select
+-- Street Fighter Wiki - Street Fighter 2 The World Warrior : https://streetfighter.fandom.com/wiki/Street_Fighter_II:_The_World_Warrior
+-- Street Fighter 2 Turbo Hyper Fighting (SNES) - World of Longplays : https://www.youtube.com/watch?v=8Usyp6OBAa8
+-- The Spriter Ressources - SF2 (SNES) Player Select Sprites : https://www.spriters-resource.com/snes/streetfighteriistreetfighteriiturbo/sheet/80834/
 
 -- CONFIG --------------------------------------------------------------------------------------------------------------
 
@@ -11,6 +15,7 @@ love.graphics.setDefaultFilter("nearest")
 
 -- MODULES -------------------------------------------------------------------------------------------------------------
 
+local my_game_settings = require("game_settings")
 local my_characters = require("characters")
 local my_character_selection_menu = require("character_selection_menu")
 
@@ -18,6 +23,11 @@ local my_character_selection_menu = require("character_selection_menu")
 
 function love.load()
     love.window.setTitle("Street Fighter character selection menu by IAmTerror")
+
+    love.window.setMode(my_game_settings.screen_width * my_game_settings.scale_factor, my_game_settings.screen_height * my_game_settings.scale_factor)
+
+    WIDTH = love.graphics.getWidth()
+    HEIGHT = love.graphics.getHeight()
 end
 
 function love.update(dt)
@@ -26,7 +36,7 @@ end
 function love.draw()
 end
 
-  -- CONTROLS ------------------------------------------------------------------------------------------------------------
+-- CONTROLS ------------------------------------------------------------------------------------------------------------
 function love.keypressed(key)
     my_characters.change()
     if key=="escape" then love.event.quit() end
