2f347f45a6bd188fbe78b5d9282a4956b34f9d89
IAmTerror <cyrilberthout@gmail.com>
2022-03-15 23:56:03 +0100

    refactoring

--- player_select_menu.lua
+++ player_select_menu.lua
@@ -9,9 +9,12 @@ local player_select_menu = {}
 
 player_select_menu.main_tileset = nil
 player_select_menu.selector_tileset = nil
-player_select_menu.overlay = nil
-player_select_menu.characters_box_selection = nil
-player_select_menu.selector = nil
+player_select_menu.overlay = {}
+player_select_menu.overlay.texture = nil
+player_select_menu.characters_box_selection = {}
+player_select_menu.characters_box_selection.texture = nil
+player_select_menu.selector = {}
+player_select_menu.selector.texture = nil
 
 
 -- FUNCTIONS -----------------------------------------------------------------------------------------------------------
@@ -29,41 +32,41 @@ function player_select_menu.selector_tileset_load()
 end
 
 function player_select_menu.overlay_load()
-    player_select_menu.overlay = love.graphics.newQuad(0, 0,
-                                                       my_game_settings.screen_width,
-                                                       my_game_settings.screen_height,
-                                                       player_select_menu.main_tileset:getDimensions())
+    player_select_menu.overlay.texture = love.graphics.newQuad(0, 0,
+                                                               my_game_settings.screen_width,
+                                                               my_game_settings.screen_height,
+                                                               player_select_menu.main_tileset:getDimensions())
 end
 
 function player_select_menu.characters_box_selection_load()
     -- TODO: décaler la box vers la gauche après placement des vignettes de personnages
-    player_select_menu.characters_box_selection = love.graphics.newQuad(110, 498,
-                                                                        121, 65,
-                                                                        player_select_menu.main_tileset_2:getDimensions())
+    player_select_menu.characters_box_selection.texture = love.graphics.newQuad(110, 498,
+                                                                                121, 65,
+                                                                                player_select_menu.main_tileset_2:getDimensions())
 end
 
 function player_select_menu.selector_load()
-    player_select_menu.selector = love.graphics.newQuad(0, 0,
-                                                        24, 36,
-                                                        player_select_menu.selector_tileset:getDimensions())
+    player_select_menu.selector.texture = love.graphics.newQuad(0, 0,
+                                                                24, 36,
+                                                                player_select_menu.selector_tileset:getDimensions())
 end
 
 function player_select_menu.overlay_draw()
-    love.graphics.draw(player_select_menu.main_tileset, player_select_menu.overlay,
+    love.graphics.draw(player_select_menu.main_tileset, player_select_menu.overlay.texture,
                         0, 0,
                         nil,
                         my_game_settings.scale_factor, my_game_settings.scale_factor)
 end
 
 function player_select_menu.characters_box_selection_draw()
-    love.graphics.draw(player_select_menu.main_tileset_2, player_select_menu.characters_box_selection,
+    love.graphics.draw(player_select_menu.main_tileset_2, player_select_menu.characters_box_selection.texture,
                        80 * my_game_settings.scale_factor, 144 * my_game_settings.scale_factor,
                        nil,
                        my_game_settings.scale_factor, my_game_settings.scale_factor)
 end
 
 function player_select_menu.selector_draw()
-    love.graphics.draw(player_select_menu.selector_tileset, player_select_menu.selector,
+    love.graphics.draw(player_select_menu.selector_tileset, player_select_menu.selector.texture,
                        80 * my_game_settings.scale_factor, 140 * my_game_settings.scale_factor,
                        nil,
                        my_game_settings.scale_factor, my_game_settings.scale_factor)
