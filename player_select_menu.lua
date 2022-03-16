7b2e1cfc4bdfc678d21448022052b18b35aea8e5
IAmTerror <cyrilberthout@gmail.com>
2022-03-16 22:40:22 +0100

    remove overlay + draw world map

--- player_select_menu.lua
+++ player_select_menu.lua
@@ -9,12 +9,19 @@ local player_select_menu = {}
 
 player_select_menu.main_tileset = nil
 player_select_menu.selector_tileset = nil
-player_select_menu.overlay = {}
-player_select_menu.overlay.texture = nil
+
 player_select_menu.characters_box_selection = {}
 player_select_menu.characters_box_selection.texture = nil
+player_select_menu.characters_box_selection.x = 80
+player_select_menu.characters_box_selection.y = 144
 player_select_menu.selector = {}
 player_select_menu.selector.texture = nil
+player_select_menu.selector.x = 80
+player_select_menu.selector.y = 140
+player_select_menu.world_map = {}
+player_select_menu.world_map.texture = nil
+player_select_menu.world_map.x = 39
+player_select_menu.world_map.y = 39
 
 
 -- FUNCTIONS -----------------------------------------------------------------------------------------------------------
@@ -23,26 +30,15 @@ function player_select_menu.main_tileset_load()
     player_select_menu.main_tileset = love.graphics.newImage("img/sf2_snes_player_select_tileset.png")
 end
 
-function player_select_menu.main_tileset_2_load()
-    player_select_menu.main_tileset_2 = love.graphics.newImage("img/sf2_snes_player_select_tileset_2.png")
-end
-
 function player_select_menu.selector_tileset_load()
     player_select_menu.selector_tileset = love.graphics.newImage("img/sf2_snes_selector_tileset.png")
-end
-
-function player_select_menu.overlay_load()
-    player_select_menu.overlay.texture = love.graphics.newQuad(0, 0,
-                                                               my_game_settings.screen_width,
-                                                               my_game_settings.screen_height,
-                                                               player_select_menu.main_tileset:getDimensions())
 end
 
 function player_select_menu.characters_box_selection_load()
     -- TODO: décaler la box vers la gauche après placement des vignettes de personnages
     player_select_menu.characters_box_selection.texture = love.graphics.newQuad(110, 498,
                                                                                 121, 65,
-                                                                                player_select_menu.main_tileset_2:getDimensions())
+                                                                                player_select_menu.main_tileset:getDimensions())
 end
 
 function player_select_menu.selector_load()
@@ -51,23 +47,32 @@ function player_select_menu.selector_load()
                                                                 player_select_menu.selector_tileset:getDimensions())
 end
 
-function player_select_menu.overlay_draw()
-    love.graphics.draw(player_select_menu.main_tileset, player_select_menu.overlay.texture,
-                        0, 0,
-                        nil,
-                        my_game_settings.scale_factor, my_game_settings.scale_factor)
+function player_select_menu.world_map_load()
+    player_select_menu.world_map.texture = love.graphics.newQuad(279, 492,
+                                                                169, 92,
+                                                                player_select_menu.main_tileset:getDimensions())
 end
 
 function player_select_menu.characters_box_selection_draw()
-    love.graphics.draw(player_select_menu.main_tileset_2, player_select_menu.characters_box_selection.texture,
-                       80 * my_game_settings.scale_factor, 144 * my_game_settings.scale_factor,
+    love.graphics.draw(player_select_menu.main_tileset, player_select_menu.characters_box_selection.texture,
+                       player_select_menu.characters_box_selection.x * my_game_settings.scale_factor,
+                       player_select_menu.characters_box_selection.y * my_game_settings.scale_factor,
                        nil,
                        my_game_settings.scale_factor, my_game_settings.scale_factor)
 end
 
 function player_select_menu.selector_draw()
     love.graphics.draw(player_select_menu.selector_tileset, player_select_menu.selector.texture,
-                       80 * my_game_settings.scale_factor, 140 * my_game_settings.scale_factor,
+                       player_select_menu.selector.x * my_game_settings.scale_factor,
+                       player_select_menu.selector.y * my_game_settings.scale_factor,
+                       nil,
+                       my_game_settings.scale_factor, my_game_settings.scale_factor)
+end
+
+function player_select_menu.world_map_draw()
+    love.graphics.draw(player_select_menu.main_tileset, player_select_menu.world_map.texture,
+                       player_select_menu.world_map.x * my_game_settings.scale_factor,
+                       player_select_menu.world_map.y * my_game_settings.scale_factor,
                        nil,
                        my_game_settings.scale_factor, my_game_settings.scale_factor)
 end
@@ -77,17 +82,16 @@ end
 
 function player_select_menu.load()
     player_select_menu.main_tileset_load()
-    player_select_menu.main_tileset_2_load()
     player_select_menu.selector_tileset_load()
-    player_select_menu.overlay_load()
     player_select_menu.characters_box_selection_load()
     player_select_menu.selector_load()
+    player_select_menu.world_map_load()
 end
 
 function player_select_menu.draw()
-    player_select_menu.overlay_draw()
     player_select_menu.characters_box_selection_draw()
     player_select_menu.selector_draw()
+    player_select_menu.world_map_draw()
 end
 
 return player_select_menu
